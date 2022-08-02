9f85f0f56a4d4f2417d014fc872de934e9132604
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-02 08:02:23 +0530

    action type in reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,8 +1,18 @@
-const intialState = {};
+const intialState = {
+  filters: [],
+  products: [],
+  tags: [],
+};
 
 function reducer(state = intialState, action) {
   console.log('reducer', action);
   switch (action.type) {
+    case 'getAllFilters':
+      return { ...state, filters: action.payload };
+    case 'getAllTags':
+      return { ...state, tags: action.payload };
+    case 'getAllProducts':
+      return { ...state, products: action.payload };
     default:
       return state;
   }
