e5dce07e9d0533b3f7eb6c60cff1b6135ffefe3e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-02 07:36:53 +0530

    imported all cmp and pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Orders from './pages/Orders';
+import Cart from './pages/Cart';
+import Logout from './pages/Logout';
+import Errorpage from './pages/Errorpage';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Header />
+      <Home />
+      <Login />
+      <Signup />
+      <Profile />
+      <Orders />
+      <Cart />
+      <Logout />
+      <Errorpage />
+      <Footer />
     </div>
   );
 }
