84ea664d80524a246fdc8f0ae6a1ccd58f889432
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-02 07:44:15 +0530

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
-
-function Header()
-{
-	return <div>
-		Header
-	</div>	
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Header() {
+  return (
+    <header>
+      <div className="logo">Ecommerce</div>
+      <div>
+        <Link to="/">home</Link>
+        <Link to="/login">login</Link>
+        <Link to="/signup">signup</Link>
+        <Link to="/profile">profile</Link>
+        <Link to="/orders">orders</Link>
+        <Link to="/cart">cart</Link>
+        <Link to="/logout">logout</Link>
+      </div>
+    </header>
+  );
 }
-export default Header
+export default Header;
