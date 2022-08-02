89e164ab910bf324f27f802d0a04871651c5b3aa
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-02 08:04:54 +0530

    got state from redux and added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,15 @@
 import React from 'react';
-
+import { useSelector, useDispatch } from 'react-redux';
 function Home() {
-  return <div>Home</div>;
+  const state = useSelector((s) => s);
+  const dispatch = useDispatch();
+  const { products, filters, tags } = state;
+  return (
+    <div>
+      <div>filters</div>
+      <div>tags</div>
+      <div>products</div>
+    </div>
+  );
 }
 export default Home;
