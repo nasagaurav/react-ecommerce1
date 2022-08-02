b63469f4ba9adc197e11e4ba3b5caf9ecbafd06e
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-02 08:30:14 +0530

    crd comp create

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-function Card() {
-  return <div>Card</div>;
+function Card(props) {
+  // console.log(props);
+
+  return (
+    <div>
+      <img width={100} height={100} src={props.image} alt="" />
+      <div>{props.title}</div>
+      <div>{props.description}</div>
+      <div>{props.old}</div>
+      <div>{props.new}</div>
+      <div>{props.rating}</div>
+      <div>{props.discount}</div>
+      <div>{props.tags}</div>
+      <div>add to cart</div>
+    </div>
+  );
 }
 export default Card;
