5544c58cfd1dc85a3159189b5856113503b4e8b7
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-02 07:40:25 +0530

    toast saetup done

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -2,10 +2,14 @@ import React from 'react';
 import store from './store';
 import App from '../App';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function Redux() {
   return (
     <Provider store={store}>
       <App />
+      <ToastContainer />
     </Provider>
   );
 }
