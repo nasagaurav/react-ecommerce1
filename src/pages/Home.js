f4ed881b567d2ee1fb7f3ddd540bce1fe8afd424
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-03 07:36:20 +0530

    added headings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,13 @@ function Home() {
   return (
     <div>
       <div>
+        <h1>filters</h1>
         {filters.map((item, index) => (
           <button key={index}>{item}</button>
         ))}
       </div>
       <div>
+        <h1>all tags </h1>
         {tags.map((item) => (
           <button key={item.t}>
             {item.t} - {item.c}
@@ -20,6 +22,7 @@ function Home() {
         ))}
       </div>
       <div>
+        <h1>all products</h1>
         {products.map((item) => (
           <Card key={item.id} {...item} />
         ))}
