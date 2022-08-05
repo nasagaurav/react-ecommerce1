6b32e99b9278a02de3fc94865f62f8d304f846d6
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-05 07:41:47 +0530

    added css flexwrap on products and now adding card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Container from 'react-bootstrap/Container';
+
 import Card from '../components/Card';
 function Home() {
   const state = useSelector((s) => s);
   const dispatch = useDispatch();
   const { products, filters, tags } = state;
   return (
-    <div>
+    <Container>
       <div>
-        <h1>filters</h1>
-        {filters.map((item, index) => (
-          <button key={index}>{item}</button>
-        ))}
+        <br />
+        <ButtonGroup aria-label="Basic example">
+          {filters.map((item, index) => (
+            <Button variant="secondary" key={index}>
+              {item}
+            </Button>
+          ))}
+          {tags.map((item) => (
+            <Button variant="primary" key={item.t}>
+              {item.t} - {item.c}
+            </Button>
+          ))}
+        </ButtonGroup>
+        <div style={{ display: 'flex', flexFlow: 'wrap', gap: '20px' }}>
+          {products.map((item) => (
+            <Card key={item.id} {...item} />
+          ))}
+        </div>
       </div>
-      <div>
-        <h1>all tags </h1>
-        {tags.map((item) => (
-          <button key={item.t}>
-            {item.t} - {item.c}
-          </button>
-        ))}
-      </div>
-      <div>
-        <h1>all products</h1>
-        {products.map((item) => (
-          <Card key={item.id} {...item} />
-        ))}
-      </div>
-    </div>
+    </Container>
   );
 }
 export default Home;
