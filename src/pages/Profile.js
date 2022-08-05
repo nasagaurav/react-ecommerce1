ff5d9e85e05ff916b92e9bbdfbb4718ba44bfc5d
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-05 07:58:59 +0530

    profile updated

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,45 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import { useSelector } from 'react-redux';
 function Profile() {
-  return <div>Profile</div>;
+  const state = useSelector((s) => s);
+  const { user } = state;
+  const { name, email, phone, password } = user;
+  return (
+    <Container>
+      <br />
+      <br />
+      <br />
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>name</Form.Label>
+          <Form.Control type="name" placeholder="Enter name" value={name} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={email} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>phone</Form.Label>
+          <Form.Control type="phone" placeholder="Enter phone" value={phone} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Update My Profile
+        </Button>
+      </Form>
+    </Container>
+  );
 }
+
 export default Profile;
