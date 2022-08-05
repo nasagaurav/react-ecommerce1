a2674573141abdda17c1ab870f8a2d0f0ce53288
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-05 07:14:37 +0530

    added css in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Redux from './redux/redux';
 
 const rootElement = document.getElementById('root');
